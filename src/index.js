a4c32f7c67cc52be941b372846a2b3253f11dfae
Georges Stefan <stefangeorges4799@gmail.com>
2022-05-22 09:14:24 -0300

    Changes ws to wss in index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import actionCable from 'actioncable';
 
 const CableApp = {};
-CableApp.cable = actionCable.createConsumer(`ws://server-back-1.herokuapp.com/cable`);
+CableApp.cable = actionCable.createConsumer(`wss://server-back-1.herokuapp.com/cable`);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
